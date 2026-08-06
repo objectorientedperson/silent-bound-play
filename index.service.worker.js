a4323ab9dd024824f6628333277d368ccc2885e6
Ali <aliyucelakgul@gmail.com>
2026-08-06 13:55:01 +0300

    build: gerçek arp (VCSL CC0), gürültü katmanları sustu

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785964251|1680895';
+const CACHE_VERSION = '1786013653|2081734';
 /** @type {string} */
 const CACHE_PREFIX = 'Silent Bound-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
